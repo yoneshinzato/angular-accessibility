fb57ff0d18d57eae1cd39dcce43ea7314cedfd22
yoneshinzato <chibariyo@gmail.com>
2022-05-19 16:20:06 -0300

    a11y completed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -37,6 +37,7 @@ import { DumplingComponent } from './shop/dumpling/dumpling.component';
 import { LocationComponent } from './location/location.component';
 import { NavigationComponent } from './navigation/navigation.component';
 import { ShopComponent } from './shop/shop.component';
+import { A11yModule } from '@angular/cdk/a11y';
 
 // TODO: #9. Add the power of @angular/cdk/a11y
 
@@ -64,6 +65,7 @@ import { ShopComponent } from './shop/shop.component';
     MatSliderModule,
     MatToolbarModule,
     GoogleMapsModule,
+    A11yModule
   ],
   providers: [Title],
   bootstrap: [AppComponent]
--- src/app/shop/color-picker/color-picker-dialog/color-picker-dialog.component.ts
+++ src/app/shop/color-picker/color-picker-dialog/color-picker-dialog.component.ts
@@ -13,8 +13,10 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { Component, EventEmitter, Inject, OnInit, Output } from '@angular/core';
 import { MatDialogRef } from '@angular/material/dialog';
+import { LiveAnnouncer } from '@angular/cdk/a11y';
+import { MAT_DIALOG_DATA } from '@angular/material/dialog';
 
 export interface ColorDialogData {
   color: string;
@@ -44,7 +46,9 @@ export class ColorPickerDialogComponent implements OnInit {
   ];
 
   // TODO: #11. Announce changes with LiveAnnouncer
-  constructor(public dialogRef: MatDialogRef<ColorPickerDialogComponent>) { }
+  constructor(public dialogRef: MatDialogRef<ColorPickerDialogComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: ColorDialogData,
+    private liveAnnouncer: LiveAnnouncer) { }
 
   ngOnInit(): void { }
 
@@ -54,6 +58,7 @@ export class ColorPickerDialogComponent implements OnInit {
     }
 
     // TODO: #11. Announce changes with LiveAnnouncer
+    this.liveAnnouncer.announce(`Selecione uma cor: ${color}`)
     this.dialogRef.close();
   }
 
